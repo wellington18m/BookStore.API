42afbe1ebf637483cdd25ba87c5a296546889dcc
Wellington Munoz <wellington18mv@gmail.com>
2021-01-03 21:28:18 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using BookStore.API.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookStore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILoggerService loggerService;
+
+        public HomeController(ILoggerService loggerService)
+        {
+            loggerService.LogInfo("Accesed Home controller");
+            this.loggerService = loggerService;
+        }
+        // GET: api/<HomeController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            loggerService.LogDebug("Get method: return Inemerable of string");
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<HomeController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<HomeController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<HomeController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+            loggerService.LogWarn("Warning: the id can not be null");
+        }
+
+        // DELETE api/<HomeController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            loggerService.LogError("The resource dos not exits");
+        }
+    }
+}
